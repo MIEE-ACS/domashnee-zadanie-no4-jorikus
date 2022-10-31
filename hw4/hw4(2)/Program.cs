73615a9e39a6d5ee4faf0731696c5b8db0ae4102
jorikus <voroschilow.eg@yandex.ru>
2022-10-31 19:24:45 +0300

    Update Program.cs

--- hw4/hw4(2)/Program.cs
+++ hw4/hw4(2)/Program.cs
@@ -18,35 +18,35 @@ namespace hw4_2_
 			}
 			return false;
 		}
-		static void Rand_array(int[,] array, int n)
+		static void Rand_array(int[,] array, int n_column, int n_string)
         {
 			Random rnd = new Random();
 			
-			for (int y = 0; y < n; y++)
+			for (int y = 0; y < n_string; y++)
             {
-				for (int x = 0; x < n; x++)
+				for (int x = 0; x < n_column; x++)
                 {
 					array[x, y] = rnd.Next(-10, 10);
                 }
             }
         }
-		static void Print_array(int[,] array, int n)
+		static void Print_array(int[,] array, int n_column, int n_string)
         {
-			for (int y = 0; y < n; y++)
+			for (int y = 0; y < n_string; y++)
 			{
-				for (int x = 0; x < n; x++)
+				for (int x = 0; x < n_column; x++)
 				{
 					Console.Write(array[x, y] + "\t");
 				}
 				Console.Write("\n");
 			}
 		}
-		static int Count_zero(int[,] array, int n)
+		static int Count_zero(int[,] array, int n_column, int n_string)
         {
 			int count = 0;
-			for (int x = 0; x < n; x++)
+			for (int x = 0; x < n_column; x++)
             {
-				for (int y = 0; y < n; y++)
+				for (int y = 0; y < n_string; y++)
                 {
 					if (array[x, y] == 0)
                     {
@@ -57,14 +57,14 @@ namespace hw4_2_
             }
 			return count;
         }
-		static int Number_string(int[,] array, int n)
+		static int Number_string(int[,] array, int n_column, int n_string)
         {
 			int max = 0;
 			int indx = 0 ;
-			for (int y = 0; y < n; y++)
+			for (int y = 0; y < n_string; y++)
             {
 				int temp_max = 0;
-				for (int x = 0; x < n - 1; x++)
+				for (int x = 0; x < n_column - 1; x++)
                 {
 					if (array[x, y] == array[x + 1, y])
                     {
@@ -88,17 +88,26 @@ namespace hw4_2_
 				Console.Write("\nОшибка. Введите число: ");
 				s = Console.ReadLine();
 			}
-			int n = int.Parse(s);
-			int[,] array2 = new int[n, n];
+			int n_column = int.Parse(s);
+			Console.Write("Введите колличество строк матрицы: ");
+			s = Console.ReadLine();
+			while (CheckNumber(s))
+			{
+				Console.Write("\nОшибка. Введите число: ");
+				s = Console.ReadLine();
+			}
+			int n_string = int.Parse(s);
 
-			Rand_array(array2, n);
+			int[,] array2 = new int[n_column, n_string];
+
+			Rand_array(array2, n_column, n_string);
 
 			Console.WriteLine("Матрица: ");
-			Print_array(array2, n);
+			Print_array(array2, n_column, n_string);
 			Console.Write("\n");
 
-			Console.WriteLine($"Количество столбцов, содержащих хотя бы один нулевой элемент :{Count_zero(array2, n)}\n");
-			Console.WriteLine($"Номер строки, в которой находится самая длинная серия одинаковых элементов :{Number_string(array2, n)}\n\n");
+			Console.WriteLine($"Количество столбцов, содержащих хотя бы один нулевой элемент :{Count_zero(array2, n_column, n_string)}\n");
+			Console.WriteLine($"Номер строки, в которой находится самая длинная серия одинаковых элементов :{Number_string(array2, n_column, n_string)}\n\n");
 
         }
     }
